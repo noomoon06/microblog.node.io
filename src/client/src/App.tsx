0e595f3e98d2671c4ffcdd4672505ae6e4125811
Moon <moonralph26@gmail.com>
2023-05-02 16:13:45 +0800

    Set initial file on the project

---
+++ src/client/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "Loading..." : data}</p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
